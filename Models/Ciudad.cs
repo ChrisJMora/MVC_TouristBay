2037530e8e9321eb16b1ade3449cfd814c18d818
Christian Jácome <christian.jacome.mora@udla.edu.ec>
2023-10-21 21:37:28 -0500

    Ciudad.cs [Añadido]

---
+++ Models/Ciudad.cs
@@ -0,0 +1,18 @@
+﻿namespace MVC_TouristBay.Models
+{
+    public class Ciudad
+    {
+        // Atributos
+        private int _idCiudad;
+        private string _nombreCiudad;
+        // Propiedades
+        public int IdCiudad { get => _idCiudad; set => _idCiudad = value; }
+        public string NombreCiudad { get => _nombreCiudad; set => _nombreCiudad = value; }
+        //Constructor
+        public Ciudad(string nombre)
+        {
+            _idCiudad = 0;
+            _nombreCiudad = nombre;
+        }
+    }
+}
