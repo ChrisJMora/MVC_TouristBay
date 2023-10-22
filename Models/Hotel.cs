b855e35375399ffe9dca52a8109a46e23b4f5925
Christian Jácome <christian.jacome.mora@udla.edu.ec>
2023-10-21 22:13:34 -0500

    Turista.cs [Añadido]

--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -11,7 +11,7 @@ namespace MVC_TouristBay.Models
         [Required]
         private string _direccionHotel;
         [Required]
-        private int _idCiudadHotel;
+        private int _ciudadHotel;
         [Required]
         private int _telefonoHotel;
         [Required]
@@ -20,7 +20,7 @@ namespace MVC_TouristBay.Models
         public int IdHotel { get => _idHotel; set => _idHotel = value; }
         public string NombreHotel { get => _nombreHotel; set => _nombreHotel = value;}
         public string DireccionHotel { get => _direccionHotel; set => _direccionHotel = value; }
-        public int IdCiudadHotel { get => _idCiudadHotel; set => _idCiudadHotel = value; }
+        public int CiudadHotel { get => _ciudadHotel; set => _ciudadHotel = value; }
         public int TelefonoHotel { get => _telefonoHotel; set => _telefonoHotel = value; }
         public int PlazasHotel { get => _plazasHotel; set => _plazasHotel = value; }
         // Constructor
@@ -29,7 +29,7 @@ namespace MVC_TouristBay.Models
             _idHotel = 0;
             _nombreHotel = nombre;
             _direccionHotel = direccion;
-            _idCiudadHotel= idCiudad;
+            _ciudadHotel= idCiudad;
             _telefonoHotel = telefono;
             _plazasHotel = plazas;
         }
---
+++ Models/Turista.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_TouristBay.Models
+{
+    public class Turista
+    {
+        // Atributos
+        private int _idTurista;
+        [Required]
+        private string _nombreTurista;
+        [Required]
+        private string _apellidoTurista;
+        [Required]
+        private string _direccionTurista;
+        [Required]
+        private int _telefonoTurista;
+        [Required]
+        private int _sucursalTurista;
+        [Required]
+        private int _hotelTurista;
+        // Propiedades
+        public int IdTurista { get => _idTurista; set => _idTurista = value; }
+        public string NombreTurista { get => _nombreTurista; set => _nombreTurista = value;}
+        public string ApellidoTurista { get => _apellidoTurista; set => _apellidoTurista = value; }
+        public string DireccionTurista { get => _direccionTurista; set => _direccionTurista = value; }
+        public int TelefonoTurista { get => _telefonoTurista; set => _telefonoTurista = value; }
+        public int SucursalTurista { get => _sucursalTurista; set => _sucursalTurista = value; }
+        public int HotelTurista { get => _hotelTurista; set => _hotelTurista = value; }
+        // Constructor
+        public Turista(string nombre, string apellido, string direccion, int telefono, int sucursal, int hotel)
+        {
+            _idTurista = 0;
+            _nombreTurista = nombre;
+            _apellidoTurista= apellido;
+            _direccionTurista = direccion;
+            _telefonoTurista= telefono;
+            _sucursalTurista= sucursal;
+            _hotelTurista= hotel;
+        }
+    }
+}
