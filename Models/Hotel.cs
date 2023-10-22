9867166aa9f163bab3b6b5d4c6a9c22e9ac84b3f
Christian Jácome <christian.jacome.mora@udla.edu.ec>
2023-10-21 21:42:00 -0500

    Hotel.cs [Actualizado - Se añade el atributo idCiudad]

--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -11,6 +11,8 @@ namespace MVC_TouristBay.Models
         [Required]
         private string _direccionHotel;
         [Required]
+        private int _idCiudadHotel;
+        [Required]
         private int _telefonoHotel;
         [Required]
         private int _plazasHotel;
@@ -18,14 +20,16 @@ namespace MVC_TouristBay.Models
         public int IdHotel { get => _idHotel; set => _idHotel = value; }
         public string NombreHotel { get => _nombreHotel; set => _nombreHotel = value;}
         public string DireccionHotel { get => _direccionHotel; set => _direccionHotel = value; }
+        public int IdCiudadHotel { get => _idCiudadHotel; set => _idCiudadHotel = value; }
         public int TelefonoHotel { get => _telefonoHotel; set => _telefonoHotel = value; }
         public int PlazasHotel { get => _plazasHotel; set => _plazasHotel = value; }
         // Constructor
-        public Hotel(string nombre, string direccion, int telefono, int plazas)
+        public Hotel(string nombre, string direccion, int idCiudad, int telefono, int plazas)
         {
             _idHotel = 0;
             _nombreHotel = nombre;
             _direccionHotel = direccion;
+            _idCiudadHotel= idCiudad;
             _telefonoHotel = telefono;
             _plazasHotel = plazas;
         }
