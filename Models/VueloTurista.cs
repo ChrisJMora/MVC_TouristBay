3503b809d9be27768147a92cea6787c18efa5c44
Christian Jácome <christian.jacome.mora@udla.edu.ec>
2023-10-21 22:18:54 -0500

    VueloTurista.cs [Añadido]

---
+++ Models/VueloTurista.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_TouristBay.Models
+{
+    public class VueloTurista
+    {
+        // Atributos
+        private int _idVueloTurista;
+        [Required]
+        private int _vuelo;
+        [Required]
+        private int _turista;
+        // Propiedades
+        public int IdVueloTurista { get => _idVueloTurista; set => _idVueloTurista = value; }
+        public int Vuelo { get => _vuelo; set => _vuelo = value; }
+        public int Turista { get => _turista; set => _turista = value; }
+        //Constructor
+        public VueloTurista(int vuelo, int turista)
+        {
+            _idVueloTurista = 0;
+            _vuelo = vuelo;
+            _turista = turista;
+        }
+    }
+}
