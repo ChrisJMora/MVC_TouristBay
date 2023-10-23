5c62f93893f491840ed5b092385c69b6b69f49d5
Christian Jácome <christian.jacome.mora@udla.edu.ec>
2023-10-23 07:20:33 -0500

    Consumo API finalizado

--- Models/Turista.cs
+++ Models/Turista.cs
@@ -11,7 +11,7 @@ namespace MVC_TouristBay.Models
         [Required]
         private string _apellidoTurista;
         [Required]
-        private string _direccionTurista;
+        private string _emailTurista;
         [Required]
         private int _telefonoTurista;
         [Required]
@@ -22,12 +22,12 @@ namespace MVC_TouristBay.Models
         public int IdTurista { get => _idTurista; set => _idTurista = value; }
         public string NombreTurista { get => _nombreTurista; set => _nombreTurista = value;}
         public string ApellidoTurista { get => _apellidoTurista; set => _apellidoTurista = value; }
-        public string DireccionTurista { get => _direccionTurista; set => _direccionTurista = value; }
+        public string EmailTurista { get => _emailTurista; set => _emailTurista = value; }
         public int TelefonoTurista { get => _telefonoTurista; set => _telefonoTurista = value; }
         public int SucursalTurista { get => _sucursalTurista; set => _sucursalTurista = value; }
         public int HotelTurista { get => _hotelTurista; set => _hotelTurista = value; }
         // Constructor
-        public Turista(string nombre, string apellido, string direccion, int telefono, int sucursal, int hotel)
+        public Turista(string nombre, string apellido, string email, string direccion, int telefono, int sucursal, int hotel)
         {
             _idTurista = 0;
             _nombreTurista = nombre;
